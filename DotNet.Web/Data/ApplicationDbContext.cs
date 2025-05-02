68a7c74736d79731d20841f28abd041ff96979d8
fahim <fahim@FAHIM>
2025-05-02 17:01:20 +0300

    Day2

---
+++ DotNet.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DotNet.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet.Web.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<People> Peoples { get; set; } 
+    }
+}
