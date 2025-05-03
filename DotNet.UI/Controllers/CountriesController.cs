329159c88444d90ef26edb2a6d49d08b51081cb3
fahim <fahim@FAHIM>
2025-05-03 10:56:11 +0300

    Day-6

--- DotNet.UI/Controllers/CountriesController.cs
+++ DotNet.UI/Controllers/CountriesController.cs
@@ -1,12 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DotNet.Entities;
+using DotNet.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DotNet.UI.Controllers
 {
     public class CountriesController : Controller
     {
+        private readonly ICountryRepo _countryRepo;
+
+        public CountriesController(ICountryRepo countryRepo)
+        {
+            _countryRepo = countryRepo;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var countries= _countryRepo.GetAll();
+            return View(countries);
+        }
+        [HttpGet]
+        public IActionResult Create() 
+        { 
+            Country country = new Country();
+            return View(country);
+        }
+        [HttpPost]
+        public IActionResult Create(Country country)
+        {
+            _countryRepo.Save(country);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var country=_countryRepo.GetById(id);
+            return View(country);
+        }
+        [HttpPost]
+        public IActionResult Edit(Country country)
+        {
+            _countryRepo.Edit(country);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var country = _countryRepo.GetById(id);
+            _countryRepo.RemoveData(country);
+            return RedirectToAction("Index");
         }
     }
 }
